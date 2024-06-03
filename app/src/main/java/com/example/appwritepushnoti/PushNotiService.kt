dceb85ed76b4b42e51939de2c243bf2c9549e8b4
Van Chuong <134tutu431@gmail.com>
2024-06-03 23:46:42 +0700

    custom sound

--- app/src/main/java/com/example/appwritepushnoti/PushNotiService.kt
+++ app/src/main/java/com/example/appwritepushnoti/PushNotiService.kt
@@ -1,54 +1,73 @@
 package com.example.appwritepushnoti
 
-import android.Manifest
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.pm.PackageManager
+import android.app.PendingIntent
+import android.content.ContentResolver
+import android.content.Intent
 import android.graphics.Color
 import android.media.AudioAttributes
 import android.net.Uri
 import android.os.Build
 import android.util.Log
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 
 class PushNotiService : FirebaseMessagingService() {
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
-        val title = message.notification?.title
-        val body = message.notification?.body
-        val CHANNEL_ID = "1234545"
-        val notificationSound = Uri.parse("android.resource://" + applicationContext.packageName + "/" + R.raw.noti_sound)
+    // Should not change the channel ID's
+    private val NOTIFICATION_EMERGENCY_CHANNEL_ID = "2002"
+    private val DEFAULT_VIBRATE_PATTERN = longArrayOf(0, 250, 250, 250)
+    private var SOUND_URI: Uri? = null
+    private var channelId: String? = null
 
-        val channel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel(
-                CHANNEL_ID,
-                "Heads Up Notification",
-                NotificationManager.IMPORTANCE_HIGH
-            ).apply {
-                setSound(notificationSound, AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_NOTIFICATION).build())
-                enableVibration(true)
-                vibrationPattern = longArrayOf(100, 200, 300, 400, 500)
-                enableLights(true)
-                lightColor = Color.RED
-            }
-        } else {
-            TODO("VERSION.SDK_INT < O")
+    override fun onMessageReceived(message: RemoteMessage) {
+        if (message.getNotification() != null) {
+            sendMyNotification(message);
         }
-        getSystemService(NotificationManager::class.java).createNotificationChannel(channel)
-        val notification = Notification.Builder(this, CHANNEL_ID)
-            .setContentTitle(title)
-            .setContentText(body.toString())
+        super.onMessageReceived(message)
+    }
+
+    private fun sendMyNotification(message: RemoteMessage) {
+        channelId = NOTIFICATION_EMERGENCY_CHANNEL_ID
+        SOUND_URI = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + packageName + "/raw/${message.data["sound"]}" )
+        Log.d("NOTIFICATION", message.toString())
+
+        val notificationBuilder: NotificationCompat.Builder = NotificationCompat.Builder(this, channelId!!)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(
+                if (message.getNotification()?.title == null) "app" else message.getNotification()!!
+                    .title
+            )
+            .setContentText(if (message.getNotification()?.body == null) "" else message.getNotification()!!.body)
             .setAutoCancel(true)
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            return
+            .setSound(SOUND_URI)
+            .setVibrate(DEFAULT_VIBRATE_PATTERN)
+
+        val mNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (SOUND_URI != null) {
+                // Changing Default mode of notification
+                notificationBuilder.setDefaults(Notification.DEFAULT_VIBRATE)
+                // Creating an Audio Attribute
+                val audioAttributes = AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+                    .build()
+
+                // Creating Channel
+                val notificationChannel = NotificationChannel(channelId, "Testing_Audio", NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.setSound(SOUND_URI, audioAttributes)
+                notificationChannel.setVibrationPattern(DEFAULT_VIBRATE_PATTERN)
+                notificationChannel.enableLights(true)
+                notificationChannel.enableVibration(true)
+                notificationChannel.lightColor = Color.RED
+                mNotificationManager.createNotificationChannel(notificationChannel)
+            }
         }
-        NotificationManagerCompat.from(this).notify(1, notification.build())
+        mNotificationManager.notify(0, notificationBuilder.build())
     }
 
     override fun onNewToken(token: String) {
