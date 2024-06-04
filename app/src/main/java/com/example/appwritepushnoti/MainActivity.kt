d0fa06218e2daf2fd3d34ae42cb71a5e64b3fec4
Van Chuong <134tutu431@gmail.com>
2024-06-04 17:31:17 +0700

    test

--- app/src/main/java/com/example/appwritepushnoti/MainActivity.kt
+++ app/src/main/java/com/example/appwritepushnoti/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.appwritepushnoti
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.ContentResolver
+import android.graphics.Color
+import android.media.AudioAttributes
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.messaging.FirebaseMessaging
 import io.appwrite.Client
@@ -16,11 +24,12 @@ import kotlinx.coroutines.tasks.await
 class MainActivity : AppCompatActivity() {
     private var token: String = ""
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
+//        createDefaultChannel()
         // Bắt đầu một coroutine để lấy token từ Firebase và gọi createPushTarget
         CoroutineScope(Dispatchers.Main).launch {
             getTokenAndCreatePushTarget()
@@ -33,7 +42,7 @@ class MainActivity : AppCompatActivity() {
             token = FirebaseMessaging.getInstance().token.await()
             Log.d("Token", token)
             // Gọi createPushTarget
-            createPushTarget()
+//            createPushTarget()
         } catch (e: Exception) {
             Log.e("Token", "Error getting Firebase token", e)
         }
@@ -50,5 +59,23 @@ class MainActivity : AppCompatActivity() {
         )
         val result = account.createPushTarget(ID.unique(), token, "663b994c00244d2f2325")
         Log.d("Token", result.toString())
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createDefaultChannel(){
+        val mNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val audioAttributes = AudioAttributes.Builder()
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .setUsage(AudioAttributes.USAGE_ALARM)
+            .build()
+
+        val notificationChannel = NotificationChannel("2002", "default", NotificationManager.IMPORTANCE_HIGH)
+        notificationChannel.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + packageName + "/raw/sound" ), audioAttributes)
+        notificationChannel.setVibrationPattern(longArrayOf(250, 250, 250, 250))
+        notificationChannel.enableLights(true)
+        notificationChannel.enableVibration(true)
+        notificationChannel.lightColor = Color.RED
+        mNotificationManager.createNotificationChannel(notificationChannel)
     }
 }
--- app/src/main/java/com/example/appwritepushnoti/PushNotiService.kt
+++ app/src/main/java/com/example/appwritepushnoti/PushNotiService.kt
@@ -18,54 +18,46 @@ import com.google.firebase.messaging.RemoteMessage
 
 class PushNotiService : FirebaseMessagingService() {
     // Should not change the channel ID's
-    private val NOTIFICATION_EMERGENCY_CHANNEL_ID = "2002"
-    private val DEFAULT_VIBRATE_PATTERN = longArrayOf(0, 250, 250, 250)
+    private val DEFAULT_VIBRATE_PATTERN = longArrayOf(250, 250, 250, 250)
     private var SOUND_URI: Uri? = null
     private var channelId: String? = null
 
     override fun onMessageReceived(message: RemoteMessage) {
-        if (message.getNotification() != null) {
-            sendMyNotification(message);
-        }
         super.onMessageReceived(message)
+        Log.d("NOTIFICATION", message.getNotification()?.title.toString())
+        sendMyNotification(message)
     }
 
     private fun sendMyNotification(message: RemoteMessage) {
-        channelId = NOTIFICATION_EMERGENCY_CHANNEL_ID
+        channelId = message.data["channel_id"]
         SOUND_URI = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + packageName + "/raw/${message.data["sound"]}" )
-        Log.d("NOTIFICATION", message.toString())
+        Log.d("NOTIFICATION", message.data.toString())
 
         val notificationBuilder: NotificationCompat.Builder = NotificationCompat.Builder(this, channelId!!)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle(
-                if (message.getNotification()?.title == null) "app" else message.getNotification()!!
-                    .title
-            )
+            .setContentTitle(if (message.getNotification()?.title == null) "app" else message.getNotification()!!.title)
             .setContentText(if (message.getNotification()?.body == null) "" else message.getNotification()!!.body)
             .setAutoCancel(true)
             .setSound(SOUND_URI)
-            .setVibrate(DEFAULT_VIBRATE_PATTERN)
 
         val mNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            if (SOUND_URI != null) {
-                // Changing Default mode of notification
-                notificationBuilder.setDefaults(Notification.DEFAULT_VIBRATE)
-                // Creating an Audio Attribute
-                val audioAttributes = AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
-                    .build()
 
-                // Creating Channel
-                val notificationChannel = NotificationChannel(channelId, "Testing_Audio", NotificationManager.IMPORTANCE_HIGH)
-                notificationChannel.setSound(SOUND_URI, audioAttributes)
-                notificationChannel.setVibrationPattern(DEFAULT_VIBRATE_PATTERN)
-                notificationChannel.enableLights(true)
-                notificationChannel.enableVibration(true)
-                notificationChannel.lightColor = Color.RED
-                mNotificationManager.createNotificationChannel(notificationChannel)
-            }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mNotificationManager.deleteNotificationChannel(channelId)
+            // Creating an Audio Attribute
+            val audioAttributes = AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .build()
+
+            // Creating Channel
+            val notificationChannel = NotificationChannel(channelId, "Testing_Audio", NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.setSound(SOUND_URI, audioAttributes)
+            notificationChannel.setVibrationPattern(DEFAULT_VIBRATE_PATTERN)
+            notificationChannel.enableLights(true)
+            notificationChannel.enableVibration(true)
+            notificationChannel.lightColor = Color.RED
+            mNotificationManager.createNotificationChannel(notificationChannel)
         }
         mNotificationManager.notify(0, notificationBuilder.build())
     }
